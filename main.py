148fe825c30750c4b356a4d8a2b3e84570ad65d5
Daniil Grigorev <grigorev.danil2003@mail.ru>
2023-06-07 21:00:24 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,16 +1,125 @@
+import itertools
 import speech_recognition as sr
 import os
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
 import moviepy.editor as mp
 
+from pydub import AudioSegment
+
+
 r = sr.Recognizer()
+converted_to_wav = "in.wav"
+
+
+def db_to_float(db, using_amplitude=True):
+    db = float(db)
+    if using_amplitude:
+        return 10 ** (db / 20)
+    else:
+        return 10 ** (db / 10)
+
+
+def split_on_silence(audio_segment, min_silence_len=1000, silence_thresh=-16, keep_silence=100,
+                     seek_step=1):
+    def pairwise(iterable):
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+    if isinstance(keep_silence, bool):
+        keep_silence = len(audio_segment) if keep_silence else 0
+
+    output_ranges = [
+        [ start - keep_silence, end + keep_silence ]
+        for (start,end)
+            in detect_nonsilent(audio_segment, min_silence_len, silence_thresh, seek_step)
+    ]
+
+    for range_i, range_ii in pairwise(output_ranges):
+        last_end = range_i[1]
+        next_start = range_ii[0]
+        if next_start < last_end:
+            range_i[1] = (last_end+next_start)//2
+            range_ii[0] = range_i[1]
+
+    return [
+        audio_segment[ max(start,0) : min(end,len(audio_segment)) ]
+        for start,end in output_ranges
+    ]
+
+
+def detect_silence(audio_segment, min_silence_len=1000, silence_thresh=-16, seek_step=1):
+    seg_len = len(audio_segment)
+    if seg_len < min_silence_len:
+        return []
+    silence_thresh = db_to_float(silence_thresh) * audio_segment.max_possible_amplitude
+    silence_starts = []
+    last_slice_start = seg_len - min_silence_len
+    slice_starts = range(0, last_slice_start + 1, seek_step)
+    if last_slice_start % seek_step:
+        slice_starts = itertools.chain(slice_starts, [last_slice_start])
+
+    for i in slice_starts:
+        audio_slice = audio_segment[i:i + min_silence_len]
+        if audio_slice.rms <= silence_thresh:
+            silence_starts.append(i)
+
+    if not silence_starts:
+        return []
+
+    silent_ranges = []
+
+    prev_i = silence_starts.pop(0)
+    current_range_start = prev_i
+
+    for silence_start_i in silence_starts:
+        continuous = (silence_start_i == prev_i + seek_step)
+
+        silence_has_gap = silence_start_i > (prev_i + min_silence_len)
+
+        if not continuous and silence_has_gap:
+            silent_ranges.append([current_range_start,
+                                  prev_i + min_silence_len])
+            current_range_start = silence_start_i
+        prev_i = silence_start_i
+
+    silent_ranges.append([current_range_start,
+                          prev_i + min_silence_len])
+
+    return silent_ranges
+
+
+def detect_nonsilent(audio_segment, min_silence_len=1000, silence_thresh=-16, seek_step=1):
+
+    silent_ranges = detect_silence(audio_segment, min_silence_len, silence_thresh, seek_step)
+    len_seg = len(audio_segment)
+
+    if not silent_ranges:
+        return [[0, len_seg]]
+
+    if silent_ranges[0][0] == 0 and silent_ranges[0][1] == len_seg:
+        return []
+
+    prev_end_i = 0
+    nonsilent_ranges = []
+    for start_i, end_i in silent_ranges:
+        nonsilent_ranges.append([prev_end_i, start_i])
+        prev_end_i = end_i
+
+    if end_i != len_seg:
+        nonsilent_ranges.append([prev_end_i, len_seg])
+
+    if nonsilent_ranges[0] == [0, 0]:
+        nonsilent_ranges.pop(0)
+
+    return nonsilent_ranges
 
 
 def transcribe_audio(path, lang):
+
     with sr.AudioFile(path) as source:
         # r.adjust_for_ambient_noise(source)
         audio_listened = r.record(source)
+        # text = r.recognize_vosk(audio_listened, language=lang)
         text = r.recognize_google(audio_listened, language=lang)
     return text
 
@@ -23,7 +132,7 @@ def splitting_by_silence(path, lang):
                               silence_thresh=sound.dBFS - 14,
                               keep_silence=200
                               )
-    folder = "audio-chunks"
+    folder = "silence-chunks"
     if not os.path.isdir(folder):
         os.mkdir(folder)
     output_text = ""
@@ -31,7 +140,7 @@ def splitting_by_silence(path, lang):
         chunk_file = os.path.join(folder, f"chunk{i}.wav")
         audio_chunk.export(chunk_file, format="wav")
         try:
-            text = transcribe_audio(chunk_file, lang)
+            text = transcribe_audio(chunk_file, lang)# [14:-3]
         except sr.UnknownValueError:
             print("No words to recognize here...")
         else:
@@ -77,4 +186,4 @@ lang = str(input("Enter the language: "))
 print("Starting convertation process...")
 wav_converter(path_to_file)
 print("Starting recognition process...")
-output.write("\nFull text:" + splitting_by_time("in.wav", lang, minutes=1 / 6))
+output.write("\nFull text:" + splitting_by_silence(converted_to_wav, lang))
